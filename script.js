2834a646a1d852073bac3d4e37b32dff3b7a255f
ChocoCodes <chocojhong999@gmail.com>
2024-10-29 19:47:57 +0800

    Task: Rounded border gradient [unfinished]

---
+++ script.js
@@ -0,0 +1,27 @@
+const days = document.querySelector('.days');
+const hours = document.querySelector('.hours');
+const mins = document.querySelector('.mins');
+const secs = document.querySelector('.secs');
+
+// Placeholder value only for LWeek Day 1
+const placeholderDate = new Date('November 18 2024 00:00:00');
+
+const updateTimer = () => {
+    const currentTime = new Date();
+    const diff = placeholderDate - currentTime;
+    const totalSeconds = Math.floor(diff / 1000);
+
+    const day = Math.floor(totalSeconds / (24 * 3600));
+    const hour = Math.floor(totalSeconds / 3600) % 24;
+    const min = Math.floor(totalSeconds / 60) % 60;
+    const sec = Math.floor(totalSeconds) % 60;
+
+    console.log(`${day} ${hour} ${min} ${sec}`);
+    days.innerHTML = day < 10 ? `0${day}` : day;
+    hours.innerHTML = hour < 10 ? `0${hour}` : hour;
+    mins.innerHTML = min < 10 ? `0${min}` : min;
+    secs.innerHTML = sec < 10 ? `0${sec}` : sec;
+
+}
+
+setInterval(updateTimer, 1000);
